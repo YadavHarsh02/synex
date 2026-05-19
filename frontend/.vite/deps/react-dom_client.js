b0e249d39b2236d05432fd4adfdc1b07d61598d7
Harsh <hy3763278@gmail.com>
2026-05-19 19:52:26 +0530

    add vercel.json rewrite rule for SPA routing

---
+++ frontend/.vite/deps/react-dom_client.js
@@ -0,0 +1,8 @@
+import {
+  require_client
+} from "./chunk-5P4VMDIW.js";
+import "./chunk-ZJYAJ63K.js";
+import "./chunk-T65XZYHU.js";
+import "./chunk-SNAQBZPT.js";
+export default require_client();
+//# sourceMappingURL=react-dom_client.js.map
